40a8972d7a0d10f87961bdd74055945e1ad5baef
Romain Delecambre <romain.delecambre@gmail.com>
2020-04-28 15:15:54 +0200

    transformation & predecoupage

--- src/htmlModele/Arrete.java
+++ src/htmlModele/Arrete.java
@@ -14,100 +14,47 @@ import okhttp3.Response;
 
 public class Arrete {
 
-	OkHttpClient client = new OkHttpClient();
-
 	private Arrete() {
 
 	}
 
-	static void test(final String cheminXML, final String cheminPDF) throws IOException {
+	static void demandeArrete(final String cheminXML, final String cheminPDF) throws IOException {
 		OkHttpClient client = new OkHttpClient().newBuilder().build();
+
 		MediaType mediaType = MediaType.parse("text/plain");
+
 		RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
 			.addFormDataPart("pdfXmlArrete", cheminXML,
-				RequestBody.create(MediaType.parse("application/octet-stream"), new File("/D:/test.xml")))
+				RequestBody.create(MediaType.parse("application/octet-stream"), new File(cheminXML)))
 			.addFormDataPart("racineFragmentsPath", "file:///mnt/NFSLOG/xl2-sir").build();
+
 		Request request = new Request.Builder()
 			.url(
 				"http://xl2-sir-edit01.sirhen.prj.in.phm.education.gouv.fr:8080/rest/services/SIRHEN_Gestion_Arretes/Processus/validerArrete:1.0")
 			.method("POST", body).addHeader("Authorization", "Basic YWRtaW5pc3RyYXRvcjpwYXNzd29yZA==").build();
 		Response response = client.newCall(request).execute();
+
 		byte[] pdf = response.body().bytes();
+		enregistreOctets(pdf, cheminPDF);
 
-		File targetFile = new File(cheminPDF);
+	}
+
+	static void enregistreOctets(final byte[] octets, final String chemin) throws IOException {
+		File targetFile = new File(chemin);
 		OutputStream outStream = new FileOutputStream(targetFile);
-		outStream.write(pdf);
+		outStream.write(octets);
 		outStream.close();
-
 	}
 
-	static void demandeArrete(final String cheminXML, final String cheminPDF) throws Exception {
-		OkHttpClient client = new OkHttpClient().newBuilder().build();
-		MediaType mediaType = MediaType.parse("text/plain");
-		RequestBody body = new MultipartBody.Builder().setType(MultipartBody.FORM)
-			.addFormDataPart("pdfXmlArrete", "/D:/test.xml",
-				RequestBody.create(MediaType.parse("application/octet-stream"), new File("/D:/test.xml")))
-			.addFormDataPart("racineFragmentsPath", "file:///mnt/NFSLOG/xl2-sir").build();
-		Request request = new Request.Builder()
-			.url(
-				"http://xl2-sir-edit01.sirhen.prj.in.phm.education.gouv.fr:8080/rest/services/SIRHEN_Gestion_Arretes/Processus/validerArrete:1.0")
-			.method("POST", body).addHeader("Authorization", "Basic YWRtaW5pc3RyYXRvcjpwYXNzd29yZA==").build();
+	static String transformeEnTexte(final String cheminPDF) throws IOException {
 
-		try (Response response = client.newCall(request).execute()) {
-			byte[] buffer = response.body().bytes();
+		File file = new File(cheminPDF);
+		PDFReader pdfReader = new PDFReader();
 
-			File targetFile = new File(cheminPDF);
-			OutputStream outStream = new FileOutputStream(targetFile);
-			outStream.write(buffer);
-			outStream.close();
-		}
+		String pdf = pdfReader.readStringContentFromFile(file);
+
+		return pdf;
 
 	}
-
-	/**
-	 * static void demandeArrete(final String cheminXml, final String cheminPdf) throws IOException {
-	 *
-	 * Charset utf8 = StandardCharsets.UTF_8;
-	 *
-	 * // PREVISUALISATION = /rest/services/SIRHEN_Gestion_Arretes/Processus/previsualiserArrete:1.0 // VALIDATION =
-	 * /rest/services/SIRHEN_Gestion_Arretes/Processus/validerArrete:1.0 String service =
-	 * "/rest/services/SIRHEN_Gestion_Arretes/Processus/validerArrete:1.0";
-	 *
-	 * byte[] message = ("administrator:password").getBytes(utf8); String auth =
-	 * javax.xml.bind.DatatypeConverter.printBase64Binary(message);
-	 *
-	 * String xml = new String(Files.readAllBytes(Paths.get(cheminXml)));
-	 *
-	 * String input = "racineFragmentsPath=" + URLEncoder.encode("file:///sirhen/datas", utf8.toString()); String xmlEncoded =
-	 * URLEncoder.encode(xml, utf8.toString()); input += "&pdfXmlArrete=" + xmlEncoded;
-	 *
-	 * // Get the file reference Path path = Paths.get("d:\\output.txt");
-	 *
-	 * // Use try-with-resource to get auto-closeable writer instance try (BufferedWriter writer = Files.newBufferedWriter(path))
-	 * { writer.write(xmlEncoded); }
-	 *
-	 * byte[] postData = input.getBytes(utf8);
-	 *
-	 * URL url = new URL("http://xl2-sir-mastt01.sirhen.prj.in.phm.education.gouv.fr:8080" + service); HttpURLConnection conn =
-	 * (HttpURLConnection) url.openConnection(); conn.setDoOutput(true); conn.setRequestMethod("POST");
-	 * conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); conn.setRequestProperty("charset",
-	 * utf8.toString()); conn.setRequestProperty("Authorization", "Basic " + auth); conn.setRequestProperty("Content-Length",
-	 * Integer.toString(postData.length));
-	 *
-	 * conn.setUseCaches(false);
-	 *
-	 * try (DataOutputStream os = new DataOutputStream(conn.getOutputStream())) { os.write(postData); }
-	 *
-	 * if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) { System.out.println(conn.getResponseMessage()); throw new
-	 * RuntimeException("Failed : HTTP error code : " + conn.getResponseCode()); }
-	 *
-	 * InputStream initialStream = conn.getInputStream(); File targetFile = new File(cheminPdf);
-	 *
-	 * java.nio.file.Files.copy(initialStream, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-	 *
-	 * conn.disconnect();
-	 *
-	 * }
-	 **/
 
 }
--- src/htmlModele/Main.java
+++ src/htmlModele/Main.java
@@ -14,9 +14,33 @@ public class Main {
 		List < Fragment > visas = Fragment.listFragments(
 			"D:\\projet-administration-des-documents\\Livraison_Fragments\\formulairesLiveCycle\\Fragment\\VisaComplementaire");
 
-		Arrete.test("D:\\test.xml", "D:\\test.pdf");
+		String cheminPDF = "D:\\test.pdf";
 
-		System.out.println("Cay fini !");
+		Arrete.demandeArrete("D:\\test.xml", cheminPDF);
+
+		String texteArrete = Arrete.transformeEnTexte(cheminPDF);
+
+		// Récupération des textes de tous les visas + tous les arrêtés
+		String debutVisas = "le recteur de l'académie d'Aix-Marseille\r\n";
+		String entreVisaEtArticle = "ARRETE\r\n";
+		String finArticles = "\r\n20 décembre 1962";
+
+		texteArrete = texteArrete.split(debutVisas, 2)[1];
+		String texteVisas = texteArrete.split(entreVisaEtArticle, 2)[0];
+		String texteArticles = texteArrete.split(entreVisaEtArticle, 2)[1];
+		texteArticles = texteArticles.split(finArticles, 2)[0];
+
+		// pour découper les lignes
+		String lines[] = texteArticles.split("\r?\n");
+		for (int i = 0; i < lines.length; ++i) {
+			System.out.println(i + " [ " + lines[i] + " ]");
+		}
+
+		// Sauvegarde sur disque
+		Arrete.enregistreOctets(texteArrete.getBytes(), "D:\\test.txt");
+
+		System.out.println("texteVisas:" + texteVisas);
+		System.out.println("texteArticles:" + texteArticles);
 
 	}
 
