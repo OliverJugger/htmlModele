b0bccf383bc5c3fb7001ca2ceb7cb2da034d9d6e
Romain Delecambre <romain.delecambre@gmail.com>
2020-04-28 10:52:27 +0200

    Arrete partie 2

--- src/htmlModele/Arrete.java
+++ src/htmlModele/Arrete.java
@@ -1,12 +1,16 @@
 package htmlModele;
 
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 public class Arrete {
@@ -15,44 +19,49 @@ public class Arrete {
 
 	}
 
-	static void valideArrete(final String cheminXml, final String cheminPdf) {
+	static void demandeArrete(final String cheminXml, final String cheminPdf) throws IOException {
 
-		String service = "rest/sync_invoke/ArreteIndividuelSIERH/ArreteIndividuelSIERH:3.0"; // Ajouter le service ici
+		Charset utf8 = StandardCharsets.UTF_8;
 
-		try {
+		// PREVISUALISATION = /rest/services/SIRHEN_Gestion_Arretes/Processus/previsualiserArrete:1.0
+		// VALIDATION = /rest/services/SIRHEN_Gestion_Arretes/Processus/validerArrete:1.0
+		String service = "/rest/services/SIRHEN_Gestion_Arretes/Processus/validerArrete:1.0";
 
-			URL url = new URL("http://xl2-sir-edit01.sirhen.prj.in.phm.education.gouv.fr:8080/" + service);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setDoOutput(true);
-			conn.setRequestMethod("POST");
-			conn.setRequestProperty("Content-Type", "application/json");
+		byte[] message = ("administrator:password").getBytes(utf8);
+		String encoded = javax.xml.bind.DatatypeConverter.printBase64Binary(message);
 
-			String input = "" + cheminXml;
+		String xml = new String(Files.readAllBytes(Paths.get(cheminXml)));
 
-			OutputStream os = conn.getOutputStream();
+		URL url = new URL("http://xl2-sir-edit01.sirhen.prj.in.phm.education.gouv.fr:8080" + service);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		conn.setRequestProperty("Authorization", "Basic " + encoded);
+
+		String input = "racineFragmentsPath=" + URLEncoder.encode("file:///mnt/NFSLOG/xl2-sir", utf8.toString());
+		// input += "&pdfXmlArrete=" + URLEncoder.encode(xml, utf8.toString());
+
+		conn.addRequestProperty("pdfXmlArrete", URLEncoder.encode(xml, utf8.toString()));
+		conn.addRequestProperty("racineFragmentsPath", URLEncoder.encode("file:///mnt/NFSLOG/xl2-sir", utf8.toString()));
+
+		// conn.setRequestProperty("Content-Length", Integer.toString(input.length()));
+
+		try (DataOutputStream os = new DataOutputStream(conn.getOutputStream())) {
 			os.write(input.getBytes());
-			os.flush();
-
-			if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
-				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
-			}
-
-			InputStream initialStream = conn.getInputStream();
-			File targetFile = new File(cheminPdf);
-
-			java.nio.file.Files.copy(initialStream, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-
-			conn.disconnect();
-
-		} catch (MalformedURLException e) {
-
-			e.printStackTrace();
-
-		} catch (IOException e) {
-
-			e.printStackTrace();
-
 		}
+
+		if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+			System.out.println(conn.getResponseMessage());
+			throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+		}
+
+		InputStream initialStream = conn.getInputStream();
+		File targetFile = new File(cheminPdf);
+
+		java.nio.file.Files.copy(initialStream, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+		conn.disconnect();
 
 	}
 
--- src/htmlModele/Main.java
+++ src/htmlModele/Main.java
@@ -1,10 +1,11 @@
 package htmlModele;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Main {
 
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws IOException {
 
 		System.out.println("Hey Roro !");
 
@@ -13,6 +14,8 @@ public class Main {
 
 		List < Fragment > visas = Fragment.listFragments(
 			"D:\\projet-administration-des-documents\\Livraison_Fragments\\formulairesLiveCycle\\Fragment\\VisaComplementaire");
+
+		Arrete.demandeArrete("D:\\test.xml", "D:\\test.pdf");
 
 	}
 
